7bf429f829296d8dbf1bd511239d4b006874d5b0
Liam Wiles <1828646+lwiles@users.noreply.github.com>
2025-01-25 16:25:53 -0600

    add Route 53 config

---
+++ route53.tf
@@ -0,0 +1,27 @@
+data "aws_route53_zone" "selected" {
+  name = var.root-domain-bucket
+}
+
+resource "aws_route53_record" "static-website" {
+  zone_id = data.aws_route53_zone.selected.zone_id
+  name    = var.root-domain-bucket
+  type    = "A"
+
+  alias {
+    name                   = aws_s3_bucket_website_configuration.website.website_endpoint
+    zone_id                = aws_s3_bucket.static_website.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "www" {
+  zone_id = data.aws_route53_zone.selected.zone_id
+  name    = var.subdomain-bucket
+  type    = "A"
+
+  alias {
+    name                   = aws_s3_bucket_website_configuration.redirect_bucket.website_endpoint
+    zone_id                = aws_s3_bucket.redirect_bucket.hosted_zone_id
+    evaluate_target_health = false
+  }
+}

902fc67a7ed404dd5af473b0280fbf149e4c4532
Liam Wiles <1828646+lwiles@users.noreply.github.com>
2025-01-25 15:34:10 -0600

    add s3 redirect bucket and re-work vars

--- s3.tf
+++ s3.tf
@@ -1,49 +1,27 @@
-# Create S3 bucket
+# Create S3 root domain bucket
 resource "aws_s3_bucket" "static_website" {
-  bucket = var.bucketname
+  bucket = var.root-domain-bucket
+}
+
+# Create redirect/subdomain bucket
+resource "aws_s3_bucket" "redirect_bucket" {
+  bucket = var.subdomain-bucket
 }
 
 resource "aws_s3_bucket_ownership_controls" "static_website" {
-    bucket = aws_s3_bucket.static_website.id
+  bucket = aws_s3_bucket.static_website.id
 
-    rule {
-        object_ownership = "BucketOwnerPreferred"
-    }
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
 }
 
 resource "aws_s3_bucket_public_access_block" "static_website" {
-  bucket = aws_s3_bucket.static_website.id
-
+  bucket                  = aws_s3_bucket.static_website.id
   block_public_acls       = false
   block_public_policy     = false
   ignore_public_acls      = false
   restrict_public_buckets = false
-}
-
-resource "aws_s3_bucket_acl" "static_website" {
-  depends_on = [
-    aws_s3_bucket_ownership_controls.static_website,
-    aws_s3_bucket_public_access_block.static_website,
-    ]
-
-  bucket = aws_s3_bucket.static_website.id
-  acl    = "public-read"
-}
-
-resource "aws_s3_object" "index" {
-  bucket = aws_s3_bucket.static_website.id
-  key    = "index.html"
-  source = "static/index.html"
-  acl = "public-read"
-  content_type = "text/html"
-}
-
-resource "aws_s3_object" "error" {
-  bucket = aws_s3_bucket.static_website.id
-  key    = "error.html"
-  source = "static/error.html"
-  acl = "public-read"
-  content_type = "text/html"
 }
 
 resource "aws_s3_bucket_website_configuration" "website" {
@@ -52,10 +30,37 @@ resource "aws_s3_bucket_website_configuration" "website" {
   index_document {
     suffix = "index.html"
   }
-
   error_document {
     key = "error.html"
   }
+}
 
-  depends_on = [ aws_s3_bucket_acl.static_website ]
+resource "aws_s3_bucket_website_configuration" "redirect_bucket" {
+  bucket = aws_s3_bucket.redirect_bucket.id
+
+  redirect_all_requests_to {
+    host_name = var.root-domain-bucket
+    protocol  = "http"
+  }
+}
+
+resource "aws_s3_bucket_acl" "static_website" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.static_website,
+    aws_s3_bucket_public_access_block.static_website,
+  ]
+
+  bucket = aws_s3_bucket.static_website.id
+  acl    = "public-read"
+}
+
+resource "aws_s3_object" "upload_object" {
+  depends_on   = [aws_s3_bucket_public_access_block.static_website]
+  for_each     = fileset("static/", "*")
+  bucket       = aws_s3_bucket.static_website.id
+  key          = each.value
+  source       = "static/${each.value}"
+  etag         = filemd5("static/${each.value}")
+  content_type = "text/html"
+  acl          = "public-read"
 }
--- variables.tf
+++ variables.tf
@@ -1,4 +1,9 @@
-variable "bucketname" {
-  type = string
-  default = "liamwiles.com"
+variable "root-domain-bucket" {
+  description = "The name of S3 bucket where the static site will be stored"
+  type        = string
+}
+
+variable "subdomain-bucket" {
+  description = "The name of the S3 bucket that will redirect to root domain bucket"
+  type        = string
 }

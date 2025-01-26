9d8dfdf8f03ca7f6a1139d284abf931968d6fe28
Liam Wiles <1828646+lwiles@users.noreply.github.com>
2025-01-25 19:44:45 -0600

    move tf provile to tfvars

--- variables.tf
+++ variables.tf
@@ -1,3 +1,8 @@
+variable "tf-profile" {
+  description = "Terraform provider profile name"
+  type        = string
+}
+
 variable "root-domain-bucket" {
   description = "The name of S3 bucket where the static site will be stored"
   type        = string
